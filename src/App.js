be0e101287871dd69f7b359bbbf68e52067f86e9
Kurisu Brooks <kurisubrooks@gmail.com>
2023-02-23 17:55:45 +1100

    Add Indicator, Schedule Day

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './styles/app.css';
+import React from 'react';
 import { Schedule } from './components/Schedule';
-
 import schedule from './data/schedule.json';
 
 const App = () => {
   return (
     <div className="app_container">
-      <Schedule schedule={schedule.THURS.schedule} />
+      <Schedule day={"THURS"} schedule={schedule} />
     </div>
   );
 }
--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,20 +1,44 @@
+import React from "react";
 import "../styles/schedule.css";
+import { ReactComponent as Indicator } from "../assets/indicator.svg";
 
 const ACTIVE_COLOUR = '#FFF0BC';
 const INACTIVE_COLOUR = '#F7E4A0';
 
-export const Schedule = ({ schedule }) => {
+const dayTransform = (day) => {
+  switch (day) {
+    case "MON":
+      return 'Monday';
+    case "TUES":
+      return 'Tuesday';
+    case "WED":
+      return 'Wednesday';
+    case "THURS":
+      return 'Thursday';
+    case "FRI":
+      return 'Friday';
+    case "SAT":
+      return 'Saturday';
+    case "SUN":
+      return 'Sunday';
+    default:
+      break;
+  }
+}
+
+export const Schedule = ({ schedule, day }) => {
   return <div className="schedule">
-    {schedule.map((item, index) =>
+    <h1 className="schedule__day" style={{ color: ACTIVE_COLOUR }}>{dayTransform(day)}</h1>
+    {schedule[day].schedule.map((item, index) =>
       <ScheduleItem
+        key={index}
         item={item}
-        isActive={index === 5} />
+        isActive={index === 4} />
     )}
   </div>;
 };
 
 const ScheduleItem = ({ item, isActive }) => {
-  console.log(item);
   const colour = isActive ? ACTIVE_COLOUR : INACTIVE_COLOUR;
   return <div className="schedule_item">
     <div
@@ -32,11 +56,8 @@ const ScheduleItem = ({ item, isActive }) => {
         ({item.text})
       </div>}
     </div>
-    <div
-      className="schedule__indicator"
-      style={{ color: colour }}
-    >
-      {isActive && "<"}
+    <div className="schedule__indicator">
+      {isActive && <Indicator fill={colour} />}
     </div>
-  </div>
+  </div>;
 }
